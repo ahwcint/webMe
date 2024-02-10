ca9587c4a84928944369b77fb766c898ab7a2aad
AUMER\aumch <chanwit.tan@everapp.io>
2024-02-10 15:49:20 +0700

    feat: login-page

--- src/components/custom/Flex.tsx
+++ src/components/custom/Flex.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import styled, { CreateStyled } from '@emotion/styled';
+import styled from '@emotion/styled';
 
 type TFlex = {
   children: React.ReactNode;
   style?: React.CSSProperties;
   direction?: React.CSSProperties['flexDirection'];
+  fullWidth?: boolean;
+  childFullWidth?: boolean;
 };
 
-export const Flex = ({ children, style, direction }: TFlex) => {
+export const Flex = ({
+  children,
+  style,
+  direction,
+  fullWidth,
+  childFullWidth,
+}: TFlex) => {
   const Flex = styled.div({
     display: 'flex',
     flexDirection: direction,
+    width: fullWidth ? '100%' : 'auto',
+    '& > *': {
+      width: childFullWidth ? '100%' : 'auto',
+    },
     ...style,
   });
   return <Flex>{children}</Flex>;
